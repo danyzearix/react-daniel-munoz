ffc796b62510e36a2438b8ff8f8b0c3bc1d80bf2
dsmr <atriasoft.ceo@gmail.com>
2022-06-06 23:41:55 -0500

    Desafio menú ecommerce

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Popper from '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

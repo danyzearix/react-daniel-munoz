c337733e1908955ca0ce3cdfbf84d3ed33a48959
dsmr <atriasoft.ceo@gmail.com>
2022-06-08 23:12:37 -0500

    Desafio crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import "./components/NavBar/NavBar"
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-    <NavBar></NavBar>
+    <div>
+      <NavBar/>
+      <ItemListContainer greeting = "Aqui van los productos" />
+    </div>
   );
 }
 

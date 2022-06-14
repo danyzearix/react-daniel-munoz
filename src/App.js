0cfbcf28f1b2ae2a2eb1efaf44826fd1f83e3d07
dsmr <atriasoft.ceo@gmail.com>
2022-06-13 23:12:53 -0500

    Desafio contador

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
+  const onAdd = (cantidad) =>{
+    console.log(`Seleccionaste ${cantidad} de unidades`)
+  }
   return (
     <div>
       <NavBar/>
       <ItemListContainer greeting = "Aqui van los productos" />
+      <ItemCount initial={0} stock={7} onAdd={onAdd}/>
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from "react"
+function ItemCount({initial, stock, onAdd}) {
+    const [count, setCount] = useState(parseInt(initial));
+    const restar = () => {
+        setCount(count - 1)
+    }
+
+    const sumar = () => {
+        setCount(count + 1)
+    }
+
+    useEffect(()=> {
+        setCount(parseInt(initial));
+    },[initial])
+
+    let min = 0;
+
+  return (
+    <div className="container-fluid d-flex flex-wrap justify-content-center mt-5 ">
+        <button disabled = {count <= min} onClick={restar} className="btn btn-primary"> - </button>
+        <span className="ms-3 me-3"> {count} </span>
+        <button disabled= {count >= stock} onClick={sumar} className="btn btn-primary"> + </button>
+            <div className="container-fluid d-flex flex-wrap justify-content-center">
+                <button disabled={stock<=0} onClick={() => onAdd(count)} className="btn btn-primary mt-1 "> Agregar al carrito</button>
+            </div>
+    </div>
+  )
+}
+
+export default ItemCount

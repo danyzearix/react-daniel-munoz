67f6ab9f8e14eb69a502c0659e67698edc3ed2e7
Dany-munoz <atriasoft.ceo@gmail.com>
2022-07-19 19:27:52 -0500

    Ordenes agregadas a firestore

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { addDoc, getFirestore, collection } from "firebase/firestore"
 import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext';
@@ -5,6 +6,25 @@ import ItemCart from '../ItemCard/ItemCart';
 
 function Cart () {
     const { cart, totalPrice } = useContext(CartContext);
+
+    const orden = {
+        cliente: {
+            name: "Daniel",
+            lastname: "Munoz",
+            email: "dgmunoz312@gmail.com",
+            phone: "3134703991",
+            address: "Cra 20 # 06-50 Soacha",       
+        },
+       // items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity })),
+        //total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const database = getFirestore();
+        const ordersCollection = collection(database, "ordenes");
+        addDoc(ordersCollection, orden)
+            .then(({id}) => console.log(id))
+    }
 
     if (cart.length === 0) {
         return (
@@ -16,7 +36,7 @@ function Cart () {
             </>
         );
     }
-        
+
   return (
     <>
         {
@@ -26,6 +46,7 @@ function Cart () {
         <h3>
             Total: ${totalPrice()}
         </h3>
+        <button onClick={handleClick}>Generar compra</button>
         </div>
         
     </>
